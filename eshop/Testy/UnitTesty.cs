59659b9cf273a5acad447661f05e11d0c90c4129
OndrejMicina <ondrej.micina@gmail.com>
2021-03-01 18:51:40 +0100

    Pridanie testu length iformfile

--- eshop/Testy/UnitTesty.cs
+++ eshop/Testy/UnitTesty.cs
@@ -76,5 +76,62 @@ namespace eshop.Testy
             //ocakava false
             Assert.False(result);
         }
+      
+
+        [Fact]
+        public void FileUploadLengthUpLimintTest()
+        {
+            //Arrange
+            var fileMock = new Mock<IFormFile>();
+            var content = "Hello World from a Fake File";
+            var fileName = "test.pdf";
+            var length = 2000001;
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+            ms.Position = 0;
+            fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
+            fileMock.Setup(_ => _.FileName).Returns(fileName);
+            fileMock.Setup(_ => _.Length).Returns(length);
+
+            var file = fileMock.Object;
+
+            FileUpload upload = new FileUpload("C:\\Users\\Ondrej\\source\\repos\\OndrejMicina\\eshop\\eshop\\wwwroot", "Tests", "image");
+
+            //Act
+            var result = upload.CheckFileLength(file);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void FileUploadLengthInLimintTest()
+        {
+            //Arrange
+            var fileMock = new Mock<IFormFile>();
+            var content = "Hello World from a Fake File";
+            var fileName = "test.pdf";
+            var length = 1999999;
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+            ms.Position = 0;
+            fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
+            fileMock.Setup(_ => _.FileName).Returns(fileName);
+            fileMock.Setup(_ => _.Length).Returns(length);
+
+            var file = fileMock.Object;
+
+            FileUpload upload = new FileUpload("C:\\Users\\Ondrej\\source\\repos\\OndrejMicina\\eshop\\eshop\\wwwroot", "Tests", "image");
+
+            //Act
+            var result = upload.CheckFileLength(file);
+
+            //Assert
+            Assert.True(result);
+        }
     }
 }
